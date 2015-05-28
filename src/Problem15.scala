cca7557d807b626093caa6eadf88339df4158e8a
Avinash <adongre@pivotal.io>
2015-05-28 17:48:30 +0530

    Solution for Problem 15

---
+++ src/Problem15.scala
@@ -0,0 +1,19 @@
+/**
+ * Created by adongre on 28/5/15.
+ */
+object Problem15 {
+    def repli[T](list: List[T], r: Int): List[T] = {
+        def repliHelper[T](lst: List[T], result: List[T]): List[T] = {
+            lst match {
+                case Nil => result
+                case x :: xs => repliHelper(xs, result ::: List.fill(r)(x))
+            }
+        }
+        repliHelper(list, Nil)
+    }
+
+    def main(args: Array[String]) {
+        println(repli(List('a', 'b', 'c'), 3))
+    }
+
+}
