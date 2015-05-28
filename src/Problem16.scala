d6dc8453c4e2ebf870226599d01d0cb441dd023e
Avinash <adongre@pivotal.io>
2015-05-28 18:51:11 +0530

    Solution for Problem 16

---
+++ src/Problem16.scala
@@ -0,0 +1,26 @@
+/**
+ * Created by adongre on 28/5/15.
+ */
+object Problem16 {
+    def dropEvery[T](input: List[T], nth: Int): List[T] = {
+        def dropHelper(list: List[T], result: List[T], index: Int): List[T] = {
+            list match {
+                case Nil => result
+                case x :: xs => {
+                    if (index == nth) {
+                        dropHelper(xs, result, 1)
+                    }
+                    else {
+                        dropHelper(xs, result ::: List(x), index + 1)
+                    }
+                }
+            }
+
+        }
+        dropHelper(input, Nil, 1)
+    }
+
+    def main(args: Array[String]) {
+        println(dropEvery(List('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k'), 3))
+    }
+}
