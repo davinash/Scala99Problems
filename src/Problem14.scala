1f673dd0fab665839b313f393b092313efc674e7
Avinash <adongre@pivotal.io>
2015-05-28 17:33:16 +0530

    Solution for Problem 14

---
+++ src/Problem14.scala
@@ -0,0 +1,19 @@
+/**
+ * Created by adongre on 28/5/15.
+ */
+object Problem14 {
+    def dupli[T](list: List[T]): List[T] = {
+        def dupliHelper[T](lst: List[T], result: List[T]): List[T] = {
+            lst match {
+                case Nil => result
+                case x :: xs => dupliHelper(xs, result ::: List(x, x) )
+            }
+        }
+
+        dupliHelper(list, Nil)
+    }
+
+    def main(args: Array[String]) {
+        println(dupli(List('a', 'b', 'c', 'c', 'd')))
+    }
+}
